c21ca3d70c3dc26709cd0a891f6096350bbb4a27
Nikolaj Slot Petersen <nikolajslot@gmail.com>
2016-03-05 09:15:21 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 var planets;
+var students;
+var URLstring = "http://aantiks.comhttp://www.ambientisotopy.com/http://ambientisotopy.comhttp://itp.aberubenste.in/http://adarshk.comhttp://adarshk.comhttp://www.sasitp.com/?p=235http://www.acoym.comhttp://www.stateofrelationships.comhttp://www.alinabalean.comhttp://www.allisonburtch.net/http://allisonevezell.comhttp://www.animishmish.comhttp://www.amandagelbitp.com/http://www.studioamelia.comhttp://www.brooklyninvention.comhttp://andrest.mehttp://andrewcerrito.com/itpbloghttp://www.ajlevine.com/itphttp://antoni.us http://asliaydin.net http://www.cargocollective.com/bengullard http://blightdesign.com/unfinished/ http://www.benedetta.cc http://www.groundlab.cc http://www.t4d.cc http://ifiwerericher.com/ http://brettjpeterson.com http://itp.briancliftonstudio.com http://www.phasesofsputnik.com http://chanwookmin.com http://www.itschristinith.com http://takinglifeseriously.com http://www.cysdavis.com http://dalitshalom.com http://itp.nyu.edu/dano http://danaras.com http://danmelancon.com http://shiffman.net/blog/ http://idlehands.tv http://www.riosriosrios.com http://davidptracy.com http://www.speakofthedevin.com http://itp.dxtr.com http://www.diegocruzcastillo.com/ http://www.dimelsamedina.com/ITPBlog/playing-with-sounds-_-icm/http://www.eamonabraham.com/http://itp.nyu.edu/~etb273/wordpress/ http://elizhang.com http://www.creative-technology.net http://erinfinnegan.com, http://stu.itp.nyu.edu/~eef206/blog/ http://www.discoverfed.com http://fletcherbach.com http://galsasson.com http://leegj.com/itp http://sciencelifeny.com http://hannahmishin.com/ http://hugoluc.com/ http://jann.ae http://itp.jasonsigal.cc http://jerllin.com http://karenlabs.com http://jingslab.com http://jiwonyoonworks.com, Portfolio : http://jiwonyoon.com http://flowersandatin.com http://www.jefarrell.com http://j-namuchoi.com http://www.jonwasserman.com http://www.Jonassp.dk http://jonathansparks.com http://www.jabrake.com http://costajoao.com http://costajoao.com http://www.juyoungp.com http://juliairwinportfolio.com http://www.moqn.net/blog/ http://itp.nyu.edu/~jl4554/blog/ http://justinr.me http://make.kategodwin.com/ http://www.katevisuth.net http://itp.katieadee.com http://www.kenamarit.info http://www.handdye.comhttp://www.kinasmith.com/itphttp://www.kristinabudelis.com/itpbloghttp://www.kristenbarry.com/http://www.kristenbarry.comhttp://kgitp.comhttp://kgitp.comhttp://www.jhclaura.com/http://leslielin.ushttp://www.lizkhoo.comhttp://www.luisaph.comhttp://www.mackhowell.comhttp://itp.nyu.edu/~msd403/blog/http://babelbubble.com/http://www.specious-space.orghttp://michaelpallison.com/http://michael.oneppo.com/blog/http://kinetec.orghttp://www.michellechandra.comhttp://www.ellecor.com/http://Www.whatthebagel.comhttp://myriam-melki.comthegirlwithstitchedeyes.comhttp://itp.nyu.edu/~nat295/blog/http://technochic.wordpress.comhttp://www.neilsolomon.me/itphttp://nevaneva.net/http://nickbratton.comhttp://omershapira.comhttp://www.patriciazablah.comhttp://itp.nyu.edu/~ps2409/Peiqi/http://www.itpme.infoitpme.ushttp://www.chenqingyuan.mehttp://lukedubois.comhttp://rodrigoderteano.comhttp://www.rodrigonarciso.com.br/bloghttp://www.roynyc.comhttp://www.ruchapatwardhan.comhttp://www.createtheunthinkable.comhttp://www.seaflection.comhttp://www.rbknrbkn.com/workandprogress/http://www.rbknrbkn.com/workandprogress/http://www.samjbrenner.comhttp://lav.iohttp://lav.iohttp://itp-sadtler.comhttp://samsadtler.comhttp://itp.nyu.edu/~sjs663/a-thinking-sam/http://itp.nyu.edu/~spp306/sanni_blog/http://sarahmak.es/http://sarahrothberg.comhttp://sarahrothberg.comhttp://scott.j38.net/http://www.thinkingclay.comhttp://s5884.com/itp/2013/12/12/icm-final/http://itp.nyu.edu/~sam926http://majluf.comhttp://sharangbiswas.prosite.comhttp://ITP.unoseistres.comhttp://www.soniali.org/artandtechhttp://kimsuhyun.com/http://www.sm-itp.com/http://suryamattu.comhttp://susanne.forchheimer.se/ITPhttp://www.teletechnophiliac.comhttp://yourfriendtk.com/ http://www.steintalya.com http://itp.nyu.edu/~tsr267/MyPOV/ http://yuplayit.com http://itp.nyu.edu/~tkh242/TBlog/ http://toddjbryant.com/itp/ http://itp.howtomworks.com http://www.howtomworks.com http://tigoe.net/blog http://tigoe.net/pcomp/code http://utt.am http://stu.itp.nyu.edu/~vjc258/valeriejeanblog/ http://vitorfreire.com http://itp.nyu.edu/~vf483/bloghttp://wdlindmeier.com/http://woonyungchoi.comhttp://woonyungchoi.com/itpbloghttp://www.tumblr.com/blog/wynaliu http://itp.nyu.edu/~wl379/blog/ http://xinranwang.com http://yangwangphilo.com/ http://yiyang-liang.com http://www.yiyang-liang.com http://yuji.io http://www.yuhu2015itper.com/portfolio/ http://feng-yuting.com http://yucef.com http://itp.nyu.edu/~yp541/blog http://www.zanderwhitehurst.com/ITP/ http://Louisminsky.com http://www.studioamelia.com http://ddaanngg.com http://stu.itp.nyu.edu/~eef206/blog/ http://songhitp.com http://ITP blog: http://www.imavahuang.com/ http://portfolio website: http://iwenhuang.com/http://peachriot.comhttp://itp.openthy.comhttp://jayatiambekar.com/http://www.vicciho.comhttp://tigoe.nethttp://tigoe.net/bloghttp://tigoe.net/pcomp/codehttp://hubertuy.comhttp://www.ranmoo.comhttp://www.hellyn.comhttp://marcabbey.comhttp://www.sabrina-osmany.comhttp://www.davidcihelna.comhttp://www.jmangoITP.comhttp://vimeo.com/adriansashttp://ipchang.nethttp://NikolajPetersen.comhttp://smalleight.comhttp://itp.coleorloff.comhttp://www.rbknrbkn.com/workandprogress/http://michael.oneppo.com/bloghttp://shilohyangzhao.comhttp://www.moniquenaoumitp.comhttp://www.moniquenaoum.comhttp://www.handdye.cohttp://www.kenamarit.infohttp://aledro.cohttp://byunnnn.comhttp://patientnkind.com/bloghttp://www.cysdavis.com/http://1023.iohttp://blightdesign.comhttp://itp.junglebrains.comhttp://Unoseistres.comhttp://ITP.unoseistres.comhttp://Madebygreg.cohttp://jasonsigal.cchttp://zivschneider.comhttp://www.wuchinhsuan.com/blog/http://rubinhuang.com/portfoliohttp://kimsehyun.krhttp://shaunaxani.com/bloghttp://www.sakihayashi.com/http://isiazu.comhttp://jffng.comhttp://www.fracoix.comhttp://zzzhen.com/http://isabelpaez88.wix.com/izapaez#!blog/c1yxuhttp://www.nevenakocic.com/http://www.yurikamulase.comhttp://www.eamonabraham.com/http://sharangbiswas.prosite.com/http://suryamattu.comhttp://www.magdalenakovarik-itp.com/http://www.ainsleyoc.com/http://www.brettstiller.comhttp://www.tommypayne.org/http://www.zklogan.nethttp://cargocollective.com/zklogan http://www.mattgoral-itp.com http://www.kellyrio.com/itp http://www.neilsolomon.me/itp http://www.constantinekoumoussis.com http://www.jaclynwickham.com/ http://www.thehypertext.com http://moqn.net http://sethkranzler.com/ http://www.iamjoanna.com/blog/ http://www.yifanhunyc.com; www.yifanhu.info http://itp.mindofmatthew.com/ http://L2inc.com http://catherine-rehwinkel.squarespace.com/itp-nyu/ http://jasonsigal.cc http://itp.nyu.edu/residents/ http://xiweihuang.com http://www.itpruta.com http://techaboom.com http://www.ishaanchaudhary.com http://itp.chino.kim http://www.joakimquach.com/blog/ http://aaronparsekian.com http://nikitahuggins.com http://carrie-yuezhang.com http://jennifertis.com http://www.gauirenata.com/ http://www.naokiishizuka.com http://doyoki.com http://jasonbeck.info http://dalitshalom.com http://streamgao.com http://iwenhuang.com/ http://songhitp.com/ http://www.davidcihelna.com http://www.liuchangitp.com http://chelseapfohl.nyc http://pancake.wtf http://yurikamulase.com http://fanhao-tseng-e39s.squarespace.com/ http://jkitppit.com http://www.creative-technology.net http://www.gabeweintraub.com http://www.zhouxiqiao.com http://katsully.com http://www.sabrinaaa.com http://abrain.in http://itp.leoneckert.com http://medium.com/luke-kao http://cargocollective.com/peiyu http://peachriot.com http://cargocollective.com/melissafelderman http://galnissim.com http://www.bradybei.com http://www.brettstiller.com http://itp.paulaceballos.com http://rebecca-ricks.com http://jordanfrand.com/ http://www.alexxu1992.com/ http://www.angelaitp.com http://jasminesoltani.com http://fracoix.com http://www.neon-sense.com http://www.ppaperplane.com http://www.gandradep.com http://wwww.nicolacarpeggiani.com http://www.yanzpro.com/ http://www.articulatedworks.com http://danielleisadora.com http://ainsleyoc.com http://sublime-sense.com http://rossgoodwin.com http://www.ellenxiliu.com/ http://www.aaronmontoyamoraga.com http://www.yitingliu.com/ http://annekgoodfriend.com/itp-blog/ http://nicole.pizza/itp http://itp.jcharry.com http://soyeonchung.net http://www.peiyu-luo.com http://www.shaunaxani.com http://www.juanjoegusquiza.com http://www.ssmultani.com http://cuevainteractive.com http://www.tanyacampbell.org http://wangxinyao-design.com/ http://www.sunwangshu.com http://mono-radio.com http://lindseyfrances.com http://www.jeddwatson.com/ http://namiraabdulgani.com/ http://www.questkennelly.com http://cargocollective.com/heying http://www.sunniewe.com/ http://dhruvdamle.com/itpblog/ http://kevings.com http://www.blondishmoment.com http://www.ondinafrate.com http://www.leslieruckman.com http://lmj.io/ http://www.katattakacs.com/itp/ http://itp.coleorloff.com http://www.artdelicorp.com/ http://fengyizhu.com http://JamieRuddyITP.com http://caiyuli.com http://zachcoble.com http://www.teletechnophiliac.com http://serenaparr.com/itp http://ndhubbard.com/itp  http://www.zhenzhenqi.info/ http://www.joyliatitp.com/ http://www.wangdanqing.com/ http://rosalieyu.com http://oryan1.com http://www.abrain.in http://dariarosedesigns.com/thoughts/ http://kainizhou.me http://jinumeli.com http://bingiology.com http://nikolajpetersen.com http://liuchangitp.com http://www.streamgao.com http://aspaceforsound.com http://tanyacampbell.org http://mayamoosh.com http://www.feldiblog.com/ http://www.sonihahn.com http://kimsehyun.kr http://playlevelone.com http://ashleyaplomb.com http://www.craigwentdigital.com/ http://www.katsully.com http://www.neveryawn.com/  http://namiraabdulgani.com/  http://karthikpatanjali.com/blogs";
+var counterurl = 1;
+
 console.log("edition 1")
 
 
@@ -7,6 +11,7 @@ function setup() {
   planets = [];
   imgitp = loadImage("ITP-Logo.png");
   blogimage = loadImage("ITP-Logo.png")
+
 }
 
 function draw() {
@@ -21,7 +26,6 @@ function draw() {
     text("Touch and give life to an ITProject!", width/2, height/2);
   }
   image(imgitp, width/2 -70, height - 120, imgitp.width/4, imgitp.height/4);
-
 }
 
 
@@ -49,14 +53,14 @@ function mousePressed() {
 }
 
 //this is the pulling and image from an RSS feed
-var pulledimg = function() { 
-  //Go through images from gss and load it in 'blogiamge'
-  return blogimage;
-}
+// var pulledimg = function() { 
+//   //Go through images from gss and load it in 'blogiamge'
+//   return blogimage;
+// }
 
-var pulledtxt = function() { 
-  return blogtxt;
-}
+// var pulledtxt = function() { 
+//   return blogtxt;
+// }
 
 
 // , pulledimg, pulledtxt
@@ -77,9 +81,32 @@ var Planet = function(pos) {
   this.previousdiameter = this.planetDiameter;
   this.expanding = false; 
 
+  //This has to do with the line
+
+
+  var urlreplace = URLstring.replace(/http/gi, ",-http");
+  var urlarray = urlreplace.split(",-");
+  //setting a randomizer aiming for current itp students.
+  
+  var urlrandomizer = floor(random(200, 380));
+  this.studentURL = urlarray[urlrandomizer]; 
+
+  console.log(counterurl);
+  this.caughtelement = document.getElementById('rank' + (counterurl));
+
+  console.log(this.caughtelement);
+  this.caughtelement.src = this.studentURL;
+  this.caughtelement.style.visibility = "visible";
+
+  this.linex = 140 + counterurl * 240 - 240;
+  this.lyney = 152; 
+  this.linexc = pos.x;
+  this.lineyc = pos.y;
+  counterurl ++;
+
   //The spawning planet equals the mouse position.
   if (random(0,1) > 0.5) {
-    this.theta = atan2(mouseX - width/2, mouseY - height);
+    this.theta = atan2(mouseX - width/2, mouseY - height);  
     this.direction = true;
   } else {
     this.theta = atan2(mouseY - height, mouseX - width/2);
@@ -91,20 +118,24 @@ Planet.prototype.run = function() {
   this.update();
   this.display();
   this.killplanet();
-  this.animate();
+  // this.animate();
 };
 
 // Method to update position
 Planet.prototype.update = function(){
   if (this.direction) {
+    this.linexc = this.r * sin(this.theta) + width/2;
+    this.lineyc = this.r * cos(this.theta) + height -60; 
     this.position = createVector(this.r * sin(this.theta) + width/2, this.r * cos(this.theta) + height -60);
   } else {
     this.position = createVector(this.r * cos(this.theta) + width/2, this.r * sin(this.theta) + height -60);
+      this.linexc = this.r * cos(this.theta) + width/2;
+    this.lineyc = this.r * sin(this.theta) + height -60;
   }
-
   this.theta += this.thetavel;
   this.lifespan -= this.thetalifespan;
   this.angle += 0.02;
+
 };
 
 Planet.prototype.animate = function() {
@@ -117,7 +148,6 @@ Planet.prototype.animate = function() {
     console.log(this.expanding);
     console.log(this.planetDiameter);
     console.log(this.previousdiameter);
-
   }
 }
 
@@ -131,6 +161,7 @@ Planet.prototype.display = function () {
   fill(255);
   textSize(32);
   text(this.clickcount, this.position.x, this.position.y);
+  line(this.linex, this.lyney, this.linexc, this.lineyc);
 };
 
 //This will check if a planet is dead
@@ -148,9 +179,9 @@ for (var i = planets.length - 1; i >= 0; i--) {
     if (planets[i].isDead()) {
       planets.splice(i, 1);
     }
+    // counterurl --;
   }
 };
-
 
   // var distance = dist(mouseX, mouseY, this.position.x, this.position.y);
   // if(distance < planets[i].planetDiameter/2) {
@@ -203,7 +234,11 @@ Planet.prototype.dosomething = function() {
  //console.log("you touched an up planet");
 }
 
+var Student = function(){ 
 
+
+
+}
 
 
 
@@ -216,6 +251,8 @@ Planet.prototype.dosomething = function() {
 // Slow down particular object when clicked and make it larger and push it a little further away from center.
 
 // Pull picture and caption from RSS feed. 
+
+
 
 
 
